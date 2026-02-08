c6277a494a9ca47057ffafe9e39fcb8e24a31a43
Yash Jadhav <yashjadhav1411@rocketmail.com>
2026-02-08 16:37:18 +0530

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import { Image, ImageBackground, StyleSheet, useWindowDimensions } from 'react-n
 
 const Page = () => {
   const { width, height } = useWindowDimensions()
+  const smokeHeight = height * 0.6
   return (
     <>
       <Canvas style={styles.canvas}>
@@ -19,6 +20,22 @@ const Page = () => {
         source={require('@/assets/images/background.png')}
         style={styles.background}
       >
+        <Canvas style={{ height: smokeHeight, ...StyleSheet.absoluteFillObject, bottom: 0, top: 'auto' }}>
+          <Rect x={0} y={0} width={width} height={smokeHeight}>
+            <LinearGradient
+              start={vec(width/2, 0)}
+              end={vec(width/2, smokeHeight)}
+              colors={['#3A3F5400', '#3A3F54']}
+              positions={[-0.02, 0.54]}
+            />
+          </Rect>
+            {/* <Line
+              p1={vec(width/2, 0)}
+              p2={vec(width/2, smokeHeight)}
+              color="green"
+              strokeWidth={10}
+            /> */}
+        </Canvas>
 
           <Image source={require('@/assets/images/house.png')}
             style={[styles.house, { width, height: width }]} />
